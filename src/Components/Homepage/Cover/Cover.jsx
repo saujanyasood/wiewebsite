a5223a002c50c26725a0b78476474df804ee2254
saujanyasood <saujanyaasood@gmail.com>
2022-01-13 23:34:07 +0530

    make some changes

--- src/Components/Homepage/Cover/Cover.jsx
+++ src/Components/Homepage/Cover/Cover.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import "./Cover.css"
+import React from "react";
+import "./Cover.css";
 
 export default function Cover() {
-    return (
-        <div className="cover" id="cover">
-            
+  return (
+    <div className="cover" id="cover">
+      <div className="title">
+        <div className="animated-title">
+          <div className="text-top">
+            <div>
+              <span>Welcome to</span>
+              <span>The WIE Chapter</span>
+            </div>
+          </div>
+          <div className="text-bottom">
+            <div>of IEEE DTU</div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
