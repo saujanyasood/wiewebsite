d63808f57f2f208e4b265aefb8a751c5c9f6aaa7
Shreya Sahu <85253740+shreyasahu4@users.noreply.github.com>
2022-01-15 18:17:20 +0530

    Update TimelineData.js

--- src/Components/Timeline/TimelineData.js
+++ src/Components/Timeline/TimelineData.js
@@ -1,45 +1,31 @@
 import google from "./images/google.png";
 import jpm from "./images/jpm.png";
-import microsoft from "./images/microsoft.png";
 import deshaw from "./images/deshaw.png";
-import apple from "./images/apple.png";
 
 export const data = [
   {
     heading: "Google Genration Scholarship",
-    description: " Women students(pursuing computer)if selected students will receive $1,000 USD",
+    description: " Women students(pursuing computer)if selected will receive $1,000 USD as Scholarship",
     img: google ,
     date: " Dec 17- Jan 4",
   },
   {
     heading: " DeShaw",
-    
+    description:" Mentorshing and Opputunity to work on tech projects"
     img: deshaw,
     date: "30 August",
   },
   {
     heading: "STEP",
-
+    description:"12 week internship for women"
     img: google,
     date: "Multiple Dates",
   },
   {
     heading: "JPMorgan Quant Mentorship",
-  
+    description:" lorem ipsum "
     img: jpm,
     date: "16 Feb",
   },
-  {
-    heading: "thing 7",
-   
-    img: microsoft,
-    date: "Feb",
-  },
-
-  {
-    heading: "thing 8 ",
-   
-    img: apple,
-    date: " March",
-  },
+ 
 ];
